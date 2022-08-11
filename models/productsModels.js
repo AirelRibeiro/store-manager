cbee9fb5bc4a4470bea52e166d05cc710dd8ce88
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-11 19:15:37 -0300

    feat [productsModels.js]: implementa funções: getAllProducts - que busca todos os produtos na tabela products; getById - que busca os produtos por id; insertProduct - que insere um produto na tabela products e retorna um objeto, contendo id do produto inserido e name

---
+++ models/productsModels.js
@@ -0,0 +1,29 @@
+const connection = require('./connection');
+
+const productsModels = {
+  getAllProducts: async () => {
+    const [products] = await connection.execute(
+      'SELECT * FROM StoreManager.products;',
+    );
+    return products;
+  },
+
+  getById: async (id) => {
+    const [product] = await connection.execute(
+        'SELECT * FROM StoreManager.products WHERE id = ?;',
+        [id],
+      );
+    return product;
+  },
+
+  insertProduct: async ({ name }) => {
+    const [{ insertId }] = await connection.execute(
+      `INSERT INTO StoreManager.products (name)
+        VALUES (?)`,
+      [name],
+    );
+    return { id: insertId, name };
+  },
+};
+
+module.exports = productsModels;
