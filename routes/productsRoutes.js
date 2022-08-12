6bcd16ae38fc65543d7c406983877a063f77360e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-12 16:47:00 -0300

    refactor: muda nome da const importada em validation

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const productsControllers = require('../controllers/productsControllers');
-const validationMiddlewares = require('../middlewares/validationMiddlewares');
+const validateName = require('../middlewares/validationMiddlewares');
 
 const productsRoutes = express.Router();
 
 productsRoutes.get('/:id', productsControllers.getById);
 
+productsRoutes.put('/:id', validateName, productsControllers.updateProduct);
+
 productsRoutes.get('/', productsControllers.getAllProducts);
 
-productsRoutes.post('/', validationMiddlewares, productsControllers.insertProduct);
-
-productsRoutes.put('/:id', validationMiddlewares, productsControllers.updateProduct);
+productsRoutes.post('/', validateName, productsControllers.insertProduct);
 
 module.exports = productsRoutes;
