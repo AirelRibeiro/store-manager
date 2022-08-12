8e7b5a2152d2f7cf7b20c51e0b0f4012f563a602
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-12 20:11:33 -0300

    feat: inclui rota para deletar produto com endpoint products/:id

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -8,6 +8,8 @@ productsRoutes.get('/:id', productsControllers.getById);
 
 productsRoutes.put('/:id', validateName, productsControllers.updateProduct);
 
+productsRoutes.delete('/:id', validateName, productsControllers.deleteProduct);
+
 productsRoutes.get('/', productsControllers.getAllProducts);
 
 productsRoutes.post('/', validateName, productsControllers.insertProduct);
