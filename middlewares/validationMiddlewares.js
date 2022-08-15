0b6eafcbd9ff016755eec9620789635e259b8295
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-15 10:35:18 -0300

    feat [validationMiddlewares.js]: estrutura objeto validationMiddlewares e inclui função salesValidations

--- middlewares/validationMiddlewares.js
+++ middlewares/validationMiddlewares.js
@@ -1,6 +1,19 @@
-const validateName = (req, res, next) => {
-  const { name } = req.body;
+const Joi = require('joi');
 
+const fieldsValidations = Joi.object({
+  productId: Joi.number().required().messages({
+    'any.required': '"productId" is required|400',
+  }),
+  quantity: Joi.number().min(1).required().messages({
+    'any.required': '"quantity" is required|400',
+    'number.min': '"quantity" must be greater than or equal to 1|422',
+  }),
+});
+
+const validationMiddlewares = {
+  validateName: (req, res, next) => {
+    const { name } = req.body;
+    
   if (!name) {
     return next({ message: '"name" is required', code: 400 });
   }
@@ -9,6 +22,22 @@ const validateName = (req, res, next) => {
   }
 
   return next();
+  },
+
+  salesValidations: (req, _res, next) => {
+    const { body } = req;
+    const result = body.map((sale) => fieldsValidations.validate(sale));
+    let validsSales = [...result];
+    if (result.some((rs) => rs.error)) {
+      result.forEach((r) => {
+        if (r.error) {
+          const [error, code] = r.error.details[0].message.split('|');
+          validsSales = { message: error, code: Number(code) };
+        }
+      });
+    }
+    return validsSales.message ? next(validsSales) : next();
+  },
 };
 
-module.exports = validateName;
+module.exports = validationMiddlewares;
