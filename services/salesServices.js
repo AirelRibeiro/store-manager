3363bc303111686b73d6155f2b045bb0e4448203
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-15 10:55:42 -0300

    feat [salesServices.js]: configura e exporta salesServices com função insertSales: função verifica validade dos productIds fornecidos e chama salesModels para incluir sales nas tabelas requeridas

---
+++ services/salesServices.js
@@ -0,0 +1,23 @@
+const salesModels = require('../models/salesModels');
+const productsModels = require('../models/productsModels');
+const thereIsProduct = require('./validations/salesValidations');
+
+const salesServices = {
+  insertSales: async (sales) => {
+    const productIds = sales.map(({ productId }) => productId);
+    const productsWithIds = await Promise.all(productIds.map((id) => productsModels.getById(id)));
+    const areProducts = thereIsProduct(productsWithIds);
+
+    if (areProducts.message) {
+      return areProducts;
+    }
+    const insertedId = await salesModels.insertSales();
+    const insertedSales = await Promise
+      .all(sales
+        .map((sale) => salesModels.insertSalesProducts(insertedId, sale.productId, sale.quantity)));
+    
+    return { id: insertedId, itemsSold: insertedSales };
+  },
+};
+
+module.exports = salesServices;
