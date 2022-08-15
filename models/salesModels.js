e49c137e194843a5e6331af7547f5bf509c581c4
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-15 15:34:58 -0300

    feat [salesModels.js]: incli função getAllSales função executa query para pegar informações de saleId, productId e quantity da tabela sales_products e date da tabela sales

--- models/salesModels.js
+++ models/salesModels.js
@@ -11,7 +11,19 @@ const salesModels = {
   insertSalesProducts: (saleId, productId, quantity) => connection.execute(
       'INSERT INTO StoreManager.sales_products (product_id, sale_id, quantity) VALUES (?, ?, ?);',
       [productId, saleId, quantity],
-    ).then(() => ({ productId, quantity })),
+  ).then(() => ({ productId, quantity })),
+  
+  getAllSales: async () => {
+    const [sales] = await connection.execute(`
+      SELECT 
+        sp.sale_id AS saleID, s.date AS date, sp.product_id AS productId, sp.quantity AS quantity
+        FROM StoreManager.sales_products AS sp
+        JOIN StoreManager.sales AS s
+        ON sp.sale_id = s.id;
+    `);
+
+    return sales;
+  },
 };
 
 module.exports = salesModels;
