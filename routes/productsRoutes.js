0f29e6881ce40fbabd54ffa5a6e150faad603802
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-15 10:41:55 -0300

    refactor [productsRoutes.js]: muda importação de validationMiddlewares para corresponder a exportação no arquivo de origem

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const productsControllers = require('../controllers/productsControllers');
-const validateName = require('../middlewares/validationMiddlewares');
+const validationMiddlewares = require('../middlewares/validationMiddlewares');
 
 const productsRoutes = express.Router();
 
 productsRoutes.get('/:id', productsControllers.getById);
 
-productsRoutes.put('/:id', validateName, productsControllers.updateProduct);
+productsRoutes.put('/:id', validationMiddlewares.validateName, productsControllers.updateProduct);
 
 productsRoutes.delete('/:id', productsControllers.deleteProduct);
 
 productsRoutes.get('/', productsControllers.getAllProducts);
 
-productsRoutes.post('/', validateName, productsControllers.insertProduct);
+productsRoutes.post('/', validationMiddlewares.validateName, productsControllers.insertProduct);
 
 module.exports = productsRoutes;
