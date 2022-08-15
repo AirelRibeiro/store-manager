5bbb699afd049c0e0dac7b23609cfa624962a9c0
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-15 17:26:53 -0300

    feat [salesControllers.js]: implementa função deleteSale: função recebe id pelo param da requisição e chama service para excluir sale, se o retorno de service for true, responde com status 204, se não, encaminha para middleware de erro

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -27,6 +27,18 @@ const salesControllers = {
 
     return res.status(200).json(sales);
   },
+
+  deleteSale: async (req, res, next) => {
+    const { id } = req.params;
+
+    const deletedSale = await salesServices.deleteSale(id);
+
+    if (deletedSale.message) {
+      return next({ message: deletedSale.message, code: 404 });
+    }
+
+    return res.status(204).end();
+  },
 };
 
 module.exports = salesControllers;
