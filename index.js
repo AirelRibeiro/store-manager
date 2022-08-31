9a514b5864377575ba86e69dc52e7deacab31061
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-31 18:18:16 -0300

    refactor: change port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log('Escutando na porta 3000');
 });
